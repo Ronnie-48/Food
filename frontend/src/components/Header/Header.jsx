5c3f8df8d62903262d0e7e157dc62586a51a993b
Ron <Ronnielangat48@gmail.com>
2024-04-30 23:14:36 +0300

    added a header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <h2>Order your favourite food here</h2>
+        <p>You select, we deliver- fast, fresh, and instant food.</p>
+      
+    </div>
+  )
+}
+
+export default Header
